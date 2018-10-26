ac4c11d9b499df043b332d53a4081f6f4a2d9b35
Michal Kokocinski <michal@waat.eu>
2018-10-26 14:47:33 +0200

    New OwlCarousel component

---
+++ src/components/OwlCarousel/OwlCarousel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+/**
+ * OWL Carousel 2 is a jQuery library and Fronthack supports it only for static
+ * HTML version. Use another libaries for React.
+ */
+class OwlCarousel extends Component {
+  render() {
+    return(
+      <p>No support for React!</p>
+    )
+  }
+}
+
+export default OwlCarousel
