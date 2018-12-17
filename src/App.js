b1594a7071327fd99d210291c3bb2d6644e2ddd9
Michal Kokocinski <michal@waat.eu>
2018-12-17 15:45:38 +0100

    Fix project and add eslint configuration

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import './style/index.sass';
-import Alert from './components/Alert';
-import Accordion from './components/Accordion';
-import Dropdown from './components/Dropdown';
-import Field from './components/Field';
-import Icon from './components/Icon';
-import Button from './components/Button';
-import Badge from './components/Badge';
-import HamburgerNav from './components/HamburgerNav';
-import Stepper from './components/Stepper';
-import LanguageSelect from './components/LanguageSelect';
-import ListingItem from './components/ListingItem';
-import Breadcrumb from './components/Breadcrumb';
-import Logo from './components/Logo';
-import Modal from './components/Modal';
-import NavHorizontal from './components/NavHorizontal';
-import NavSubway from './components/NavSubway';
-import Pagination from './components/Pagination';
-import Parallax, { ParallaxGroup } from './components/Parallax';
-import ProgressBar from './components/ProgressBar';
-import Rating from './components/Rating';
-import SocialLinks from './components/SocialLinks';
-import Tabs from './components/Tabs';
-import { Form, FormInput } from './components/form';
+import React, { Fragment } from 'react'
+import logo from './logo.svg'
+import './style/index.sass'
+import Alert from './components/Alert'
+import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
+import Field from './components/Field'
+import Icon from './components/Icon'
+import Button from './components/Button'
+import Badge from './components/Badge'
+import HamburgerNav from './components/HamburgerNav'
+import Stepper from './components/Stepper'
+import LanguageSelect from './components/LanguageSelect'
+import ListingItem from './components/ListingItem'
+import Breadcrumb from './components/Breadcrumb'
+import Logo from './components/Logo'
+import Modal from './components/Modal'
+import NavHorizontal from './components/NavHorizontal'
+import NavSubway from './components/NavSubway'
+import Pagination from './components/Pagination'
+import Parallax, { ParallaxGroup } from './components/Parallax'
+import ProgressBar from './components/ProgressBar'
+import Rating from './components/Rating'
+import SocialLinks from './components/SocialLinks'
+import Tabs from './components/Tabs'
+import { Form, FormControl } from './components/Form'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Tabs labels={['First', 'Second', 'Third']}>
           {[
             <div>Content of the first tab</div>,
@@ -50,7 +50,7 @@ class App extends React.Component {
             url: '#',
           },
         ]}>Toggle dropdown</Dropdown>
-        <SocialLinks url={window.location.href } />
+        <SocialLinks url={window.location.href} />
         <Rating value={1} />
         <ProgressBar percentage={45} />
         <NavHorizontal links={[
@@ -97,9 +97,9 @@ class App extends React.Component {
           },
         ]}/>
         <Modal
-          size="lg"
-          title="Modal title"
-          trigger={<Button variant="primary">Open modal</Button>}
+          size='lg'
+          title='Modal title'
+          trigger={<Button variant='primary'>Open modal</Button>}
           // isOpen
         >
           Modal content
@@ -107,11 +107,11 @@ class App extends React.Component {
         <Logo />
         <Breadcrumb items={['Docs', 'Components API', 'Button']} />
         <ListingItem
-          id="12345"
-          title="Featured article name"
-          date="16 October 2018"
-          description="Teaser text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut."
-          mediaUrl="http://placehold.it/200x145"
+          id='12345'
+          title='Featured article name'
+          date='16 October 2018'
+          description='Teaser text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.'
+          mediaUrl='http://placehold.it/200x145'
         />
         <LanguageSelect
           languages={[
@@ -124,9 +124,9 @@ class App extends React.Component {
               callback: () => alert('Callback. It can be history.push or anything'),
             },
           ]}
-          current="English"
+          current='English'
         />
-        <Alert message="Example message" type="success"/>
+        <Alert message='Example message' type='success'/>
         <Stepper
           steps={['Cart', 'Shipping', 'Billing', 'Summary', 'Complete']}
           current={2}
@@ -147,80 +147,80 @@ class App extends React.Component {
           },
         ]} />
         <div>Test<Badge number={7}/></div>
-        <div><Icon type="bell" /><Badge number={7}/></div>
-        <Field label="Field label" spacing values={[
+        <div><Icon type='bell' /><Badge number={7}/></div>
+        <Field label='Field label' spacing values={[
           'Multiple value',
           'Another value',
           'Third value',
         ]}/>
-        <Field label="Field label" inlineLabel>Single value</Field>
+        <Field label='Field label' inlineLabel>Single value</Field>
         <Form fields={['username', 'framework', 'color', 'car']}>
-          <FormInput
-            fieldname="username"
-            label="User name"
-            type="text"
-            placeholder="Enter username"
-            initialHelp="Required field"
+          <FormControl
+            fieldname='username'
+            label='User name'
+            type='text'
+            placeholder='Enter username'
+            initialHelp='Required field'
             required
           />
-          <FormInput
-            fieldname="framework"
-            label="Select a Framework"
-            type="select"
+          <FormControl
+            fieldname='framework'
+            label='Select a Framework'
+            type='select'
             options={[
               { label: 'React', value: 'react' },
               { label: 'Vue', value: 'vue' },
               { label: 'Angluar', value: 'angluar' },
             ]}
-            initialHelp="Select from the list"
+            initialHelp='Select from the list'
           />
-          <FormInput
-            fieldname="car"
-            label="Select cars"
-            type="checkbox"
+          <FormControl
+            fieldname='car'
+            label='Select cars'
+            type='checkbox'
             options={[
               { label: 'Toyota', value: 'toyota' },
               { label: 'Renault', value: 'renault' },
               { label: 'Volkswagen', value: 'volkswagen' },
             ]}
-            initialHelp="You can pick a few"
+            initialHelp='You can pick a few'
           />
-          <FormInput
-            fieldname="color"
-            label="Pick a color"
-            type="radio"
+          <FormControl
+            fieldname='color'
+            label='Pick a color'
+            type='radio'
             options={[
               { label: 'Red', value: 'red' },
               { label: 'Yellow', value: 'yellow' },
               { label: 'Blue', value: 'blue' },
             ]}
-            initialHelp="Choose one"
+            initialHelp='Choose one'
           />
         </Form>
         <Parallax>
-          <ParallaxGroup className="parallax__demo-solid">
-            <div class="parallax_demo-title">Solid base layer</div>
+          <ParallaxGroup className='parallax__demo-solid'>
+            <div className='parallax_demo-title'>Solid base layer</div>
           </ParallaxGroup>
           <ParallaxGroup backLayer={
             <Fragment>
-              <img class="full-width" src="https://placeimg.com/640/480/nature" alt="" />
-              <div class="parallax_demo-title">Back layer</div>
+              <img className='full-width' src='https://placeimg.com/640/480/nature' alt='' />
+              <div className='parallax_demo-title'>Back layer</div>
             </Fragment>
           }>
-            <div class="parallax_demo-title">Transparent base layer</div>
+            <div className='parallax_demo-title'>Transparent base layer</div>
           </ParallaxGroup>
-          <ParallaxGroup className="parallax__demo-solid">
-            <div class="parallax_demo-title">Solid base layer</div>
+          <ParallaxGroup className='parallax__demo-solid'>
+            <div className='parallax_demo-title'>Solid base layer</div>
           </ParallaxGroup>
-          <ParallaxGroup className="parallax__demo-solid">
-            <div class="parallax_demo-title">Solid base layer</div>
+          <ParallaxGroup className='parallax__demo-solid'>
+            <div className='parallax_demo-title'>Solid base layer</div>
           </ParallaxGroup>
         </Parallax>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
