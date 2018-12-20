75a9b7baf69ac3d3880bd82d3b0b9b468afac43f
Michal Kokocinski <michal@waat.eu>
2018-12-20 17:43:49 +0100

    Update Tooltip component

--- src/components/Tooltip/EXAMPLE.js
+++ src/components/Tooltip/EXAMPLE.js
@@ -1,4 +1,8 @@
 import React from 'react'
 import Tooltip from '.'
+import Icon from '../Icon'
 
-export default () => <Tooltip message="Hey, I'm made with pure CSS" direction='tr' />
+export default () =>
+  <Tooltip message="Hey, I'm made with pure CSS" direction='br'>
+    <Icon type='question-mark' />
+  </Tooltip>
--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import bemCx from 'bem-modifiers'
 import './style.sass'
 
 
-class Tooltip extends React.Component {
-  render() {
-    const { direction, message, children } = this.props
-    return (
-      <div className={bemCx('tooltip', direction)} data-tooltip={message}>
-        {children}
-      </div>
-    )
-  }
-}
+const Tooltip = ({ direction, message, children }) =>
+  <div className={bemCx('tooltip', direction)} data-tooltip={message}>
+    {children}
+  </div>
 
 Tooltip.defaultProps = {
   direction: 'tl',
+}
+
+Tooltip.PropTypes = {
+  direction: PropTypes.oneOf(['tl', 'tr', 'bl', 'br']),
+  message: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
 }
 
 export default Tooltip
