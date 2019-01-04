7c2b214687cb088aad6b98652c94eacd95e5115d
Michal Kokocinski <michal@waat.eu>
2019-01-04 13:19:58 +0100

    Docs - Introduction chapter

--- src/pages/DocsIntroduction.js
+++ src/pages/DocsIntroduction.js
@@ -1,5 +1,31 @@
 import React from 'react'
 
 export default () => <div>
-  <p>Coming soon...</p>
+  <h2>What Fronthack is?</h2>
+
+  <p>Fronthack is a complete solution for coding a frontend for the app or a static HTML site. With help of Fronthack it is easier to build scallable projects based on React, Next.js or just a static HTML. It provides a powerfull commandline generator tool and a design system. Which help of it you can generate an opiniated bolerplate for new project, use a wide library of components that are ready to use (like buttons, modals or more complex hamburger menus or forms) or create own custom components that share the unified approach. In the end it results in very good structure of your project.</p>
+
+  <p>Fronthack also contains a tool that allows you to implement designs with a pixel perfect precision.</p>
+
+  <h2>Generate and hack</h2>
+
+  <p>Fronthack utilizes the <strong>Generate and hack</strong> approach. What is the meaning behind that? The world of design systems and npm packages provides us a variety of ready to use components, which makes things easier to setup, but in most cases styling them in a way that corresponds to our designs might be a bit triky. Usually you import a component from its npm package and that complicates its customization. Some of design systems enforce designer to use imposed styles. Fronthack is style agnostic. Suggested components are very basic and easy to modify. Just generate a component from Fronthack repository of templates, then modify it as you like. That what is called "generate and hack".</p>
+
+  <h2>What Fronthack includes?</h2>
+
+  <ul>
+    <li>A boilerplate for three types of projects (React, Next.js or static HTML) with Webpack or Gulp under the hood.</li>
+    <li>Styles written in Sass.</li>
+    <li>Library of ready components which you add to your project and modify.</li>
+    <li>Global Sass variables used by all components, that gives them common sense of style.</li>
+    <li>Tool that allows you to display designs under the HTML code in a browser.</li>
+    <li>Tool to visualy plan the UI structure, before writing any code.</li>
+    <li>Linter configuration.</li>
+    <li>Opiniated file structure of the project.</li>
+    <li>And more...</li>
+  </ul>
+
+  <h2>What you can find in these docs</h2>
+
+  <p>This documentation will guide you trough whole process of creating a project using Fronthack, step by step. After read you will understad it’s concept and how to use it for your projects.</p>
 </div>
