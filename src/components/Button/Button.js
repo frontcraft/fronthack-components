9346ce1d404804f4e06e14377c1b56f3cf97b4d3
Michal Kokocinski <michal@waat.eu>
2019-08-05 12:36:56 +0200

    Add component prop to button and imprive its styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,27 +7,37 @@ import './style.sass'
 const Button = ({
   children,
   onClick,
+  component,
   variant,
   size,
   block,
   disabled,
   className,
-}) =>
-  <div
-    className={bemCx('btn', {
-      [variant]: variant,
-      [size]: size,
-      block: block,
-      disabled: disabled,
-    }, { [className]: className })}
-    onClick={onClick}
-  >{children}</div>
+}) => {
+  const ComponentProp = component
+  return (
+    <ComponentProp
+      className={bemCx('btn', {
+        [variant]: variant,
+        [size]: size,
+        block: block,
+        disabled: disabled,
+      }, { [className]: className })}
+      onClick={onClick}
+    >{children}</ComponentProp>
+  )
+}
+
+Button.defaultProps = {
+  component: 'button',
+}
 
 Button.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
+  component: PropTypes.node,
   onClick: PropTypes.func,
   variant: PropTypes.oneOf([
     'primary',
